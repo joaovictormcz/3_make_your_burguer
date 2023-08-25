7b45efc09a01d26cf7c351a1258bd6745ffe22fd
joaovictormcz <joaovictor.mcz@gmail.com>
2023-08-25 16:53:51 -0300

    Aula 22

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <NavbarComponent/>
+  <NavbarComponent :logo="logo_src" :alt="app_name"/>
   <router-view/>
   <FooterComponent/>
 </template>
@@ -13,11 +13,34 @@
     components: {    
     NavbarComponent,
     FooterComponent
-}
+    },
+    data(){
+      return {
+        logo_src: "/img/logo.png",
+        app_name: "Make Your Burguer"
+      }
+    }
   }
 
 </script>
 
 <style>
+  * {
+    font-family: Helvetica;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
 
+  .main-container {
+    margin: 50px;
+    min-height: 250px;
+  }
+
+  h1 {
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222;
+  }   
 </style>
