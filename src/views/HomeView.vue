5a4a4cd4fd2a1d5b477abe8b04337b8608a914ef
joaovictormcz <joaovictor.mcz@gmail.com>
2023-08-29 15:50:28 -0300

    Aula 25

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <template>
+  <BannerComponent />  
   <div class="main-container">
-    <h1>HOME</h1>
+    <h1>Monte o seu burger:</h1>
+    <BurgerForm />
   </div>
 </template>
 
 <script>
+  import BannerComponent from '@/components/BannerComponent.vue';
+  import BurgerForm from '@/components/BurgerForm.vue';
+
+  export default {
+    name: "HomeView",
+    components: {
+      BannerComponent,
+      BurgerForm
+    }
+  }
+  
 
 </script>
