96c1debc23c99cc1f84dd37547d505190e578096
joaovictormcz <joaovictor.mcz@gmail.com>
2023-09-18 14:22:50 -0300

    Curso Vue

--- src/views/PedidosView.vue
+++ src/views/PedidosView.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="main-container">
-    <h1>PEDIDOS</h1>
+    <h1>Gerenciar pedidos:</h1>
+    <DashboardComponent/>
   </div>
 </template>
+
+<script>
+
+  import DashboardComponent from '../components/DashboardComponent.vue'  
+
+  export default {
+    name: "PedidosView",
+    components: {
+      DashboardComponent
+    }
+  }
+
+</script>
